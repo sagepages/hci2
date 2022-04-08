5a5c575cd7d15730679c4466947387595a1db883
sagepages <62083371+sagepages@users.noreply.github.com>
2022-04-08 17:45:26 -0400

    helper changes

--- helperfunctions.py
+++ helperfunctions.py
@@ -17,3 +17,11 @@ def findFrequency(mapJson):
             except:
                 pokemon_type_dist[type_dict[j]] = 1
     return pokemon_type_dist
+
+def spreadData(graph_data, df, poke_selection):
+
+    poke_row = df.loc[df['Name'] == poke_selection]
+    graph_data[0] = poke_row.iloc[0]['Base Stamina']
+    graph_data[1] = poke_row.iloc[0]['Base Attack']
+    graph_data[2] = poke_row.iloc[0]['Base Defense']
+
